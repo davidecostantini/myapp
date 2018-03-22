9e23bd616da643b1acdc88d56ef78aa4cd230abb
Davide 3500 <me@davidecostantini.it>
2018-03-22 09:52:13 +0000

    Added comment to build script

--- build_image.sh
+++ build_image.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+echo "Building the docker image"
 docker build -t mikyrospetto/condenast .
+
+echo "Now the image should goes trough some tests and pushed to the repo with appropriate tag"
